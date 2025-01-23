9a500298db6e470d96ad83b6cc1d39651c936b04
Andrew Shiroma <amshiroma@zoho.com>
2025-01-23 15:16:32 -0800

    finished mission08

--- drones_class/mission08.py
+++ drones_class/mission08.py
@@ -3,7 +3,7 @@ import logging
 
 from djitellopy import Tello
 from dragonflight import Dragon
-import tello_sim
+# import tello_sim
 
 def mission08():
     mission_params = {
@@ -15,14 +15,14 @@ def mission08():
         'min_operating_power': 10,
     }
     
-    # mytello = Tello()
-    mytello = tello_sim.DroneSim()
+    mytello = Tello()
+    # mytello = tello_sim.DroneSim()
     drone = Dragon(mytello, mission_params, logging.WARNING)
     
     drone.takeoff()
-    drone.rotate_ccw(34)
+    drone.rotate_ccw(345)
     # drone.rotate_to_bearing(30 + (180 * 3))
-    drone.fly_to_coordinates(-149, 263, False)
+    drone.fly_to_coordinates(313, -157, False)
     # drone.fly_to_coordinates(0, 0, True)
     # drone.fly_home()
 
