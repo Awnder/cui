d7b678ff912e34a1929b1699beb10cdb157aed39
Awnder <amshiroma@zoho.com>
2025-01-24 22:03:02 -0800

    added wasd controls

--- drones_class/pygame_visual.py
+++ drones_class/pygame_visual.py
@@ -55,10 +55,11 @@ while running:
 
         # Special one-time keys: SPACE -> 'boom' and ESC -> Quit
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                print("boom")
+            if event.key == pygame.K_c:
+                pass # turn on camera
+
             if event.key == pygame.K_ESCAPE:
-                running = False            
+                running = False  
 
         # One of the most common mouse events
         if event.type == pygame.MOUSEBUTTONDOWN:
@@ -73,21 +74,53 @@ while running:
     keys = pygame.key.get_pressed()
 
     # Full throttle all the time!
-    if keys[pygame.K_UP] and keys[pygame.K_DOWN]:
+    if (keys[pygame.K_UP] or keys[pygame.K_w]) and (keys[pygame.K_DOWN] or keys[pygame.K_s]):
         velocity_x = 0
-    elif keys[pygame.K_UP]:
+    elif (keys[pygame.K_UP] or keys[pygame.K_w]):
         velocity_x = 100
-    elif keys[pygame.K_DOWN]:
+    elif (keys[pygame.K_DOWN] or keys[pygame.K_s]):
         velocity_x = -100
     else:
         velocity_x = 0
 
+    if (keys[pygame.K_LEFT] or keys[pygame.K_a]) and (keys[pygame.K_RIGHT] or keys[pygame.K_d]):
+        velocity_y = 0
+    elif (keys[pygame.K_LEFT] or keys[pygame.K_a]):
+        velocity_y = 100
+    elif (keys[pygame.K_RIGHT] or keys[pygame.K_d]):
+        velocity_y = -100
+    else:
+        velocity_y = 0
+
+    if keys[pygame.K_SPACE] and keys[pygame.K_LCTRL]:
+        velocity_z = 0
+    elif keys[pygame.K_SPACE]:
+        velocity_z = 100
+    elif keys[pygame.K_LCTRL]:
+        velocity_z = -100
+    else:
+        velocity_z = 0
+
+
     # Send command to drone
     # drone.send_rc_control(velocity_x, velocity_y, velocity_z, rotation)
+
+    # get drone states
+    # battery = drone.get_battery()
+    # height = drone.get_height()
+    # temp = drone.get_temperature()
+    
+    battery_surface = font.render(str(54), True, COLOR_GREEN, COLOR_GRAY)
+    battery_rect = battery_surface.get_rect()
+    # height = 50
+    # temp = 30
 
     # Place surfaces on the screen but don't display them (order matters)
     # Internally this will use depth calculations to create the final image
     screen.blit(background, (0, 0))
+    screen.blit(battery_surface, (10,10))
+    # screen.blit(height, (10, 40))
+    # screen.blit(temp, (10, 70))
     screen.blit(logo_surface, logo_rect)
 
 ################################################################################
