2bc23cf968a5f587061d14f6cda7e3d01dc430c2
Andrew Shiroma <amshiroma@zoho.com>
2025-02-27 16:21:08 -0800

    added custom data sampling

--- ml/handwriting_recognition/handwriting_recognition.py
+++ ml/handwriting_recognition/handwriting_recognition.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 import numpy as np
 import tkinter as tk
+from tkinter import ttk
 from PIL import Image, ImageTk, ImageGrab, ImageOps
 from sklearn.datasets import fetch_openml
 from sklearn.metrics import accuracy_score, classification_report
@@ -11,15 +12,15 @@ from sklearn.neighbors import KNeighborsClassifier
 
 
 class HandwritingRecognition:
-    def __init__(self):
+    def __init__(self, custom_data=False):
         self.black_pixel = np.full((1, 3), 0, dtype=np.uint8)
         self.white_pixel = np.full((1, 3), 255, dtype=np.uint8)
         self.test_sample = np.full((140, 140, 3), self.black_pixel, dtype=np.uint8)
-        self.X, self.y = self.fetch_mnist()
+        self.X, self.y = self.fetch_custom_data() if custom_data else self.fetch_mnist() 
         self.rf, self.knn = self.train_models()
         self.last_x, self.last_y = None, None
 
-    def fetch_mnist(self):
+    def fetch_mnist(self) -> tuple[np.ndarray, np.ndarray]:
         """Fetches the MNIST dataset from OpenML."""
         mnist = None
         if os.path.exists("mnist_784.csv"):
@@ -34,7 +35,17 @@ class HandwritingRecognition:
 
         return X, y
 
-    def train_models(self):
+    def fetch_custom_data(self) -> tuple[np.ndarray, np.ndarray]:
+        """Fetches custom data from a CSV file."""
+        X, y = None, None
+        if os.path.exists("custom_data.csv"):
+            data = np.loadtxt("custom_data.csv", delimiter=",")
+            X = data[:, 1:].astype(np.uint8)
+            y = data[:, 0].astype(np.uint8)
+
+        return X, y
+
+    def train_models(self) -> tuple[RandomForestClassifier, KNeighborsClassifier]:
         """Finds best hyperparameters and trains RandomForest on the MNIST dataset."""
         X_train, X_test, y_train, y_test = train_test_split(self.X, self.y)
 
@@ -49,8 +60,8 @@ class HandwritingRecognition:
 
         return rf, knn
 
-    def guess_digit(self, drawspace, main_pred_lbl, pred_lbls):
-        """Guesses the digit drawn on the canvas and displays the prediction."""
+    def canvas_to_array(self, drawspace: tk.Canvas) -> tk.Image:
+        """Captures, grayscales, and resizes the image drawn on the canvas."""
         x = drawspace.winfo_rootx()
         y = drawspace.winfo_rooty()
         width = drawspace.winfo_width()
@@ -60,24 +71,25 @@ class HandwritingRecognition:
         image = ImageGrab.grab(bbox=(x, y, x + width, y + height))
         image = image.convert("L")
         image = ImageOps.invert(image) # invert tkinter black and white
+        image = image.resize((28, 28), Image.Resampling.LANCZOS) # high quality resizing
 
-        # Display the image in a new window
+        # display the captured image
         # new_window = tk.Toplevel()
         # new_window.title("Captured Image")
         # new_window.geometry("200x200")
-
         # img = ImageTk.PhotoImage(image.resize((200, 200), Image.Resampling.LANCZOS))
         # panel = tk.Label(new_window, image=img)
         # panel.image = img  # keep a reference to avoid garbage collection
         # panel.pack()
 
-        image = image.resize((28, 28), Image.Resampling.LANCZOS) # high quality resizing
-
         numpy_array = np.array(image)
         numpy_array = (numpy_array > 128).astype(np.uint8) * 255  # 0 black, 255 white
         numpy_array = numpy_array.reshape(-1, 784)
+        return numpy_array
 
-        print(numpy_array)
+    def guess_digit(self, drawspace: tk.Canvas, main_pred_lbl: tk.Label, pred_lbls: tuple[tk.Label]) -> None:
+        """Guesses the digit drawn on the canvas and displays the prediction."""
+        numpy_array = self.canvas_to_array(drawspace)
 
         pred_rf = self.rf.predict(numpy_array)
         neighbors = self.knn.kneighbors(numpy_array, n_neighbors=3, return_distance=False)
@@ -94,17 +106,26 @@ class HandwritingRecognition:
         pred_lbls[1].image = neighbor_imgs[1]  # keep a reference to avoid garbage collection
         pred_lbls[2].config(image=neighbor_imgs[2])
         pred_lbls[2].image = neighbor_imgs[2]  # keep a reference to avoid garbage collection
+        
 
-    def clear_drawing(self, drawspace):
+    def add_custom_data(self, drawspace: tk.Canvas, custom_data_combobox: ttk.Combobox) -> None:
+        """Adds custom data to the CSV file."""
+        numpy_array = self.canvas_to_array(drawspace)
+
+        with open("custom_data.csv", "a") as f:
+            f.write(f"{custom_data_combobox.get()},{','.join(map(str, numpy_array[0]))}\n")
+
+    def clear_drawing(self, drawspace: tk.Canvas) -> None:
         """Clears the drawing canvas and internal array."""
         drawspace.delete("all")  # clear canvas
         self.test_sample[:] = self.black_pixel  # internal representation of drawing
 
-    def start_drawing(self, event):
+    def start_drawing(self, event: tk.Event) -> None:
+        """Sets the initial coordinates of the mouse when drawing."""
         self.last_x = event.x
         self.last_y = event.y
 
-    def draw(self, event, drawspace):
+    def draw(self, event: tk.Event, drawspace: tk.Canvas) -> None:
         """Draws on the canvas and internal array based on mouse coordinates."""
         # xy-coordinates and radius of the circle being drawn
         drawspace.create_line(
@@ -120,7 +141,8 @@ class HandwritingRecognition:
         self.last_x = event.x
         self.last_y = event.y
 
-    def draw_window(self):
+    def draw_window(self) -> None:
+        """Creates the drawing window and all its components."""
         window = tk.Tk()
         window.geometry("400x400")
         window.wm_title("Drawing Canvas")
@@ -140,11 +162,16 @@ class HandwritingRecognition:
         pred2_lbl = tk.Label(window, image=None)
         pred3_lbl = tk.Label(window, image=None)
         pred_lbls = [pred1_lbl, pred2_lbl, pred3_lbl]
-        btn_clear = tk.Button(
+        custom_data_combobox = ttk.Combobox(window, values=["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"], state="readonly")
+        custom_data_combobox.set("0")
+        clear_btn = tk.Button(
             window, text="Clear", command=lambda: self.clear_drawing(drawspace)
         )
-        btn_guess = tk.Button(
+        guess_btn = tk.Button(
             window, text="Guess", command=lambda: self.guess_digit(drawspace, main_pred_lbl, pred_lbls)
+        )
+        add_custom_data_btn = tk.Button(
+            window, text="Add Custom Data", command=lambda: self.add_custom_data(drawspace, custom_data_combobox)
         )
 
         drawspace.bind("<Button-1>", self.start_drawing)
@@ -152,18 +179,20 @@ class HandwritingRecognition:
 
         title_lbl.pack()
         drawspace.pack()
-        btn_clear.pack()
-        btn_guess.pack()
+        clear_btn.pack()
+        guess_btn.pack()
         main_pred_lbl.pack()
         pred1_lbl.pack(side=tk.LEFT, padx=5)
         pred2_lbl.pack(side=tk.LEFT, padx=5)
         pred3_lbl.pack(side=tk.LEFT, padx=5)
-
+        custom_data_combobox.pack()
+        add_custom_data_btn.pack()
+        
         window.resizable(False, False)
         window.mainloop()
 
 
 if __name__ == "__main__":
-    hr = HandwritingRecognition()
+    hr = HandwritingRecognition(custom_data=False)
     hr.train_models()
     hr.draw_window()
