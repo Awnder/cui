19144519ded0513a0f5d38a11e371c9ddc7f349a
Andrew Shiroma <amshiroma@zoho.com>
2025-03-03 16:59:34 -0800

    display multiple models

--- ml/handwriting_recognition/handwriting_recognition.py
+++ ml/handwriting_recognition/handwriting_recognition.py
@@ -1,5 +1,4 @@
 import os
-import asyncio
 import pandas as pd
 import numpy as np
 import tkinter as tk
@@ -17,11 +16,11 @@ class HandwritingRecognition:
         self.black_pixel = np.full((1, 3), 0, dtype=np.uint8)
         self.white_pixel = np.full((1, 3), 255, dtype=np.uint8)
         self.test_sample = np.full((140, 140, 3), self.black_pixel, dtype=np.uint8)
-        self.X, self.y = self.fetch_custom_data() if custom_data else self.fetch_mnist() 
-        self.rf, self.knn = self.train_models()
+        self.X, self.y = self._fetch_custom_data() if custom_data else self._fetch_mnist() 
+        self.rf, self.knn, self.lr = self._train_models()
         self.last_x, self.last_y = None, None
 
-    def fetch_mnist(self) -> tuple[np.ndarray, np.ndarray]:
+    def _fetch_mnist(self) -> tuple[np.ndarray, np.ndarray]:
         """Fetches the MNIST dataset from OpenML."""
         mnist = None
         if os.path.exists("mnist_784.csv"):
@@ -33,49 +32,26 @@ class HandwritingRecognition:
             X = mnist.data.astype(np.uint8)
             y = mnist.target.astype(np.uint8)
             np.savetxt("mnist_784.csv", np.column_stack((y, X)), delimiter=",", fmt='%d')
-
         return X, y
 
-    def fetch_custom_data(self) -> tuple[np.ndarray, np.ndarray]:
+    def _fetch_custom_data(self) -> tuple[np.ndarray, np.ndarray]:
         """Fetches custom data from a CSV file."""
         X, y = None, None
         if os.path.exists("custom_data.csv"):
             data = np.loadtxt("custom_data.csv", delimiter=",")
             X = data[:, 1:].astype(np.uint8)
             y = data[:, 0].astype(np.uint8)
-
         return X, y
 
-    async def train_models(self) -> tuple[RandomForestClassifier, KNeighborsClassifier, LogisticRegression]:
+    def _train_models(self) -> tuple[RandomForestClassifier, KNeighborsClassifier, LogisticRegression]:
         """Finds best hyperparameters and trains RandomForest on the MNIST dataset."""
-        # X_train, X_test, y_train, y_test = train_test_split(self.X, self.y)
-
-        rf = await self._train_random_forest()
-        knn = await self._train_knn()
-        lr = await self._train_logistic_regression()
-
-        # rf.fit(X_train, y_train)
-        # knn.fit(X_train, y_train)
-
-        # y_pred = rf.predict(X_test)
-        # print(classification_report(y_test, y_pred))
-
-        return rf, knn, lr
-    
-    async def _train_random_forest(self) -> RandomForestClassifier:
         rf = RandomForestClassifier(n_estimators=20)
         rf.fit(self.X, self.y)
-        return rf
-
-    async def _train_knn(self) -> KNeighborsClassifier:
         knn = KNeighborsClassifier(n_neighbors=3)
         knn.fit(self.X, self.y)
-        return knn
-    
-    async def _train_logistic_regression(self) -> LogisticRegression:
-        lr = LogisticRegression()
+        lr = LogisticRegression(max_iter=1000)
         lr.fit(self.X, self.y)
-        return lr
+        return rf, knn, lr
 
     def canvas_to_array(self, drawspace: tk.Canvas) -> tk.Image:
         """Captures, grayscales, and resizes the image drawn on the canvas."""
@@ -104,28 +80,31 @@ class HandwritingRecognition:
         numpy_array = numpy_array.reshape(-1, 784)
         return numpy_array
 
-    def guess_digit(self, drawspace: tk.Canvas, main_pred_lbl: tk.Label, pred_lbls: tuple[tk.Label]) -> None:
+    def guess_digit(self, drawspace: tk.Canvas, main_pred_lbl: tk.Label, pred_lbls: list[tk.Label]=None) -> None:
         """Guesses the digit drawn on the canvas and displays the prediction."""
         numpy_array = self.canvas_to_array(drawspace)
 
-        pred_rf = self.rf.predict(numpy_array)
-        neighbors = self.knn.kneighbors(numpy_array, n_neighbors=3, return_distance=False)
+        pred_rf = self.rf.predict(numpy_array)[0]
+        pred_knn = self.knn.predict(numpy_array)[0]
+        pred_lr = self.lr.predict(numpy_array)[0]
 
-        print('rf', pred_rf)
-        print('neighbors:', self.knn.predict(numpy_array))
-
-        main_pred_lbl.config(text=f"Prediction: {pred_rf[0]}")
+        print(f"Random Forest: {pred_rf}")
+        print(f"K Nearest Neighbors: {pred_knn}")
+        print(f"Logistic Regression: {pred_lr}")
         
-        # Display the top 3 neighbors
-        neighbor_image_arrays = [self.X[neighbors[0][i]].reshape(28, 28) for i in range(3)]
-        neighbor_images = [ImageTk.PhotoImage(image=Image.fromarray(img).resize((50, 50), Image.Resampling.LANCZOS)) for img in neighbor_image_arrays]
+        # boyer-moore voting algorithm to determine majority vote
+        count = 0
+        candidate = None
+        for num in [pred_rf, pred_knn, pred_lr]:
+            if count == 0:
+                candidate = num
+            count += 1 if num == candidate else -1
 
-        pred_lbls[0].config(image=neighbor_images[0])
-        pred_lbls[0].image = neighbor_images[0]  # keep a reference to avoid garbage collection
-        pred_lbls[1].config(image=neighbor_images[1])
-        pred_lbls[1].image = neighbor_images[1]
-        pred_lbls[2].config(image=neighbor_images[2])
-        pred_lbls[2].image = neighbor_images[2]
+        main_pred_lbl.config(text=f"Prediction: {candidate}")
+        if pred_lbls:
+            pred_lbls[0].config(text=f"Random Forest: {pred_rf}")
+            pred_lbls[1].config(text=f"K Nearest Neighbors: {pred_knn}")
+            pred_lbls[2].config(text=f"Logistic Regression: {pred_lr}")
 
     def add_custom_data(self, drawspace: tk.Canvas, custom_data_combobox: ttk.Combobox) -> None:
         """Adds custom data to the CSV file."""
@@ -143,6 +122,15 @@ class HandwritingRecognition:
         """Sets the initial coordinates of the mouse when drawing."""
         self.last_x = event.x
         self.last_y = event.y
+
+    def enable_multiple_pred(self, drawspace: tk.Canvas, main_pred_lbl: tk.Label, pred_lbls: list[tk.Label], multiple_pred_checkbox: ttk.Checkbutton, guess_btn: tk.Button) -> None:
+        """Enables or disables the display of multiple predictions."""
+        if multiple_pred_checkbox.instate(["selected"]):
+            guess_btn.config(command=lambda: self.guess_digit(drawspace, main_pred_lbl, pred_lbls))
+        else:
+            for pred_lbl in pred_lbls:
+                pred_lbl.config(text="")
+            guess_btn.config(command=lambda: self.guess_digit(drawspace, main_pred_lbl))
 
     def draw(self, event: tk.Event, drawspace: tk.Canvas) -> None:
         """Draws on the canvas and internal array based on mouse coordinates."""
@@ -163,7 +151,7 @@ class HandwritingRecognition:
     def draw_window(self) -> None:
         """Creates the drawing window and all its components."""
         window = tk.Tk()
-        window.geometry("400x400")
+        window.geometry("400x460")
         window.wm_title("Drawing Canvas")
 
         drawspace = tk.Canvas(
@@ -175,22 +163,23 @@ class HandwritingRecognition:
             highlightthickness=1,
             highlightbackground="steelblue",
         )
-        # imagespace = Image.new("L", size=(200, 200), color="white")
-        # imagespace_draw = ImageDraw.Draw(imagespace)
 
         title_lbl = tk.Label(window, text="Draw a digit", font=("Helvetica", 16))
-        main_pred_lbl = tk.Label(window, text="", font=("Helvetica", 16))
-        pred1_lbl = tk.Label(window, image=None)
-        pred2_lbl = tk.Label(window, image=None)
-        pred3_lbl = tk.Label(window, image=None)
-        pred_lbls = [pred1_lbl, pred2_lbl, pred3_lbl]
+        main_pred_lbl = tk.Label(window, text="Prediction: ", font=("Helvetica", 14))
+        rf_pred_lbl = tk.Label(window, text="", font=("Helvetica", 10), state="disabled")
+        knn_pred_lbl = tk.Label(window, text="", font=("Helvetica", 10), state="disabled")
+        lr_pred_lbl = tk.Label(window, text="", font=("Helvetica", 10), state="disabled")
+
+        multiple_pred_checkbox = ttk.Checkbutton(window, text="Show Multiple Predictions", variable=tk.BooleanVar(), command=lambda: self.enable_multiple_pred(drawspace, main_pred_lbl, [rf_pred_lbl, knn_pred_lbl, lr_pred_lbl], multiple_pred_checkbox, guess_btn))
+        
         custom_data_combobox = ttk.Combobox(window, values=["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"], state="readonly")
         custom_data_combobox.set("0")
+        
         clear_btn = tk.Button(
             window, text="Clear", command=lambda: self.clear_drawing(drawspace)
         )
         guess_btn = tk.Button(
-            window, text="Guess", command=lambda: self.guess_digit(drawspace, main_pred_lbl, pred_lbls)
+            window, text="Guess", command=lambda: self.guess_digit(drawspace, main_pred_lbl, [rf_pred_lbl, knn_pred_lbl, lr_pred_lbl])
         )
         add_custom_data_btn = tk.Button(
             window, text="Add Custom Data", command=lambda: self.add_custom_data(drawspace, custom_data_combobox)
@@ -201,20 +190,20 @@ class HandwritingRecognition:
 
         title_lbl.pack()
         drawspace.pack()
-        clear_btn.pack()
-        guess_btn.pack()
         main_pred_lbl.pack()
-        pred1_lbl.pack(side=tk.LEFT, padx=5)
-        pred2_lbl.pack(side=tk.LEFT, padx=5)
-        pred3_lbl.pack(side=tk.LEFT, padx=5)
+        guess_btn.pack(pady=2)
+        clear_btn.pack(pady=2)
         custom_data_combobox.pack()
         add_custom_data_btn.pack()
-        
+        multiple_pred_checkbox.pack()
+        rf_pred_lbl.pack()
+        knn_pred_lbl.pack()
+        lr_pred_lbl.pack()
+
         window.resizable(False, False)
         window.mainloop()
 
 
 if __name__ == "__main__":
     hr = HandwritingRecognition(custom_data=False)
-    hr.train_models()
     hr.draw_window()
